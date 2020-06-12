810fc77dd54c10abd725b7a06863f6b7d9587cb7
Tillo <tillo.bosshart@gmail.com>
2020-06-12 07:35:55 +0200

    WIP

--- config/server.go
+++ config/server.go
@@ -1,11 +1,27 @@
 package config
 
+import (
+	"os"
+	"github.com/morgulbrut/colorlog"
+	"github.com/morgulbrut/toml"
+)
+
 type Path struct{
-	file string,
+	file string
 	response int
 }
 
 type Server struct{
-	Port string,
+	Port string
 	Paths []Path
+}
+
+func Read(conffile string) Server {
+	var conf Server
+	_, err := toml.DecodeFile(conffile, &conf)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	return conf
 }
